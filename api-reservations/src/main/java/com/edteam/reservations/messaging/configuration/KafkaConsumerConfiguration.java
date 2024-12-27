6dd71ac27877c979029bb9dd80832f0009682dc6
borispacex <borisvargaspaucara@gmail.com>
2024-12-27 07:03:14 -0400

    curso finalizado

--- api-reservations/src/main/java/com/edteam/reservations/messaging/configuration/KafkaConsumerConfiguration.java
+++ api-reservations/src/main/java/com/edteam/reservations/messaging/configuration/KafkaConsumerConfiguration.java
@@ -1,13 +1,18 @@
 package com.edteam.reservations.messaging.configuration;
 
+import com.edteam.reservations.dto.PaymentDTO;
+import com.edteam.reservations.dto.ReservationTransactionDTO;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,16 +21,57 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConsumerConfiguration {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Value(value = "${spring.kafka.consumer.group-id}")
+    private String consumerGroupId;
+
+    @Value(value = "${spring.kafka.consumer.client-id}")
+    private String consumerClientId;
+
+    @Value(value = "${spring.kafka.properties.schema.registry.url}")
+    private String schemaRegistryURL;
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
+    public ConsumerFactory<String, PaymentDTO> consumerPaymentConsumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+        configProps.put(ConsumerConfig.CLIENT_ID_CONFIG, consumerClientId);
+        configProps.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
+        configProps.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL);
+
+        return new DefaultKafkaConsumerFactory<>(configProps);
     }
 
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PaymentDTO> consumerListenerPaymentConsumerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, PaymentDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerPaymentConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, ReservationTransactionDTO> consumerReservationTransactionConsumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+        configProps.put(ConsumerConfig.CLIENT_ID_CONFIG, consumerClientId);
+        configProps.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
+        configProps.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL);
+
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ReservationTransactionDTO> consumerListenerReservationTransactionConsumerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, ReservationTransactionDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerReservationTransactionConsumerFactory());
+        return factory;
+    }
 }
